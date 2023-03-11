aea0b93d494bdb1796563414c4bcb3d700ab8dfd
VenturaGiu <giulia-ratti@hotmail.com>
2023-03-11 12:20:35 -0300

    :recycle: REFACTOR: atualização de versões

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
-export function Home(){
+export function Home() {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Teste</Text>
         </View>
     );
 }
